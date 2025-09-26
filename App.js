98aabab1f3f8d8e5d98efafca8c19d90030fecf0
Thiago Santos <thiagodlsantos9@gmail.com>
2025-09-25 21:35:32 -0300

    feat: add audio resources functionalities

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { CameraView, useCameraPermissions } from "expo-camera";
+import { useEffect, useState } from "react";
+import { Audio } from "expo-av";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
+  const [permission, requestPermission] = useCameraPermissions();
+  const [sound, setSound] = useState(null);
+
+  const playAudio = async () => {
+    if (sound) {
+      await sound.unloadAsync();
+      setSound(null);
+    }
+
+    const { sound: newSound } = await Audio.Sound.createAsync(
+      require("./assets/audio.mp3")
+    );
+    setSound(newSound);
+    await newSound.playAsync();
+  };
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
+  if (!permission) {
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    return (
+      <View style={styles.center}>
+        <Text>Precisamos da permissão da câmera</Text>
+        <TouchableOpacity onPress={requestPermission} style={styles.button}>
+          <Text style={styles.label}>Conceder permissão</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CameraView style={styles.camera}>
+        <TouchableOpacity style={styles.overlay} onPress={playAudio}>
+          <Text style={styles.label}>🎵 Toque na imagem</Text>
+        </TouchableOpacity>
+      </CameraView>
     </View>
   );
 }
@@ -13,8 +57,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  camera: {
+    flex: 1,
+  },
+  center: { flex: 1, alignItems: "center", justifyContent: "center" },
+  button: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: "blue",
+    borderRadius: 5,
+  },
+  text: { color: "#fff" },
+  overlay: {
+    position: "absolute",
+    bottom: 100,
+    alignSelf: "center",
+    backgroundColor: "rgba(0,0,0,0.6)",
+    padding: 15,
+    borderRadius: 10,
+  },
+  label: { color: "#fff", fontSize: 16 },
 });
